69ce3f6973ef5711d25836f3f9cc13ff72adcec7
AJ-533 <82697189+AJ-533@users.noreply.github.com>
2021-05-12 19:54:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function updateRate() {
+    var rate = document.getElementById("myrange").value;
+    document.getElementById("val").innerText = rate + "%";
+    document.getElementById("myrange").oninput = function () {
+        document.getElementById("val").innerText = this.value;
+    }
+
 }
-        
+
+function compute() {
+    var principal = document.querySelector("#principal").value;
+    if (principal < 0) {
+        window.alert("Enter positive principal");
+        return document.getElementById("principal").focus();
+    }
+
+    var rate = document.querySelector("#myrange").value;
+    var years = document.querySelector("#yrs").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    document.getElementById("res").style.marginLeft = "41.5%";
+    document.querySelector("#result").innerHTML = "\<br\><br\>If you deposit <mark>" + principal + "</mark>,\<br\><br\>at an interest rate of <mark>" + rate + "%</mark>\<br\><br\>You will receive an amount of <mark>" + interest + "</mark>,\<br\><br\>in the year <mark>" + year + "</mark>\<br\>";
+}
